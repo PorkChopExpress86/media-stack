e67313dedebb39dc789fb3cfba2684c0831b0354
PorkChopExpress86 <29725019+PorkChopExpress86@users.noreply.github.com>
2025-01-13 13:04:52 -0600

    powershell script to update images and log when run

--- update-containers.ps1
+++ update-containers.ps1
@@ -1,1 +1,18 @@
-docker compose pull; docker compose down; docker compose up -d
+
+function Update-Compose{
+    Set-Location -Path "C:\Users\Blake\OneDrive\Desktop\Docker"
+    docker compose pull; docker compose down; docker compose up -d
+}
+
+function Log-Run {
+    $logFilePath = "C:\Users\Blake\OneDrive\Desktop\Docker\file.log"
+    if(!(Test-Path $logFilePath)) {
+        New-Item -ItemType File -Path $logFilePath
+    }
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    Add-Content -Path $logFilePath -Value "$timestamp - Update Compose has been run."
+}
+
+Log-Run
+Update-Compose
+
